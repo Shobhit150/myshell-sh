69b0599cc1d756d5d503db041caca8fe783e03e5
Shobhit150 <vshobhit150@gmail.com>
2026-02-14 22:02:23 +0530

    feat: exit mechanism

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <string>
 
-int main(){
+int main() {
+    // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    while (true){
+    // TODO: Uncomment the code below to pass the first stage
+    while (true) {
         std::cout << "$ ";
         std::string command;
         std::getline(std::cin, command);
+        if (command == "exit") {
+            break;
+        }
         std::cout << command << ": command not found" << "\n";
     }
 }
