4b50d123a4eb26f6959d67e4acdb496eef85d30d
Shobhit150 <vshobhit150@gmail.com>
2026-02-14 21:42:10 +0530

    feat: infinite

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@ int main(){
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    std::cout << "$ ";
-    std::string command;
-    std::getline(std::cin, command);
+    while (true){
+        std::cout << "$ ";
+        std::string command;
+        std::getline(std::cin, command);
+        std::cout << command << ": command not found" << "\n";
+    }
 }
