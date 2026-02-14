acfccc72ee9fecc611f02d666600d8323be7e46a
Shobhit150 <vshobhit150@gmail.com>
2026-02-14 21:31:58 +0530

    feat: invalid command

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main() {
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+int main(){
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+    std::cout << "$ ";
+    std::string command;
+    std::getline(std::cin, command);
 }
